701621c2b49d1dfc26e5d38659201025e5b8ec00
Yuumicoin <yuumisolanacoin@gmail.com>
2025-07-24 15:55:39 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#5C6FC3',
+        accent: '#F0C0DD',
+      },
+    },
+  },
+  plugins: [],
+};
